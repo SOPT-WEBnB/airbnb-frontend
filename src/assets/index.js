a4a9f0633a9debe853cd4deda095afe63b1cf324
100Gyeon <bjy9966@naver.com>
2022-05-16 14:15:12 +0900

    chore: 필요한 asset 추가

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,2 +1,10 @@
-export { default as icSample } from './icons/ic_sample.svg';
-export { default as imgSample } from './images/img_sample.svg';
+export { default as icBack } from './icons/ic_back.svg';
+export { default as icCalendar } from './icons/ic_calendar.svg';
+export { default as icClose } from './icons/ic_close.svg';
+export { default as icFillHeart } from './icons/ic_fill_heart.svg';
+export { default as icFillHeartBtn } from './icons/ic_fill_heart_btn.svg';
+export { default as icUnfillHeartBtn } from './icons/ic_unfill_heart_btn.svg';
+export { default as icMedal } from './icons/ic_medal.svg';
+export { default as icPin } from './icons/ic_pin.svg';
+export { default as icPlus } from './icons/ic_plus.svg';
+export { default as imgRoom } from './images/img_room.svg';
