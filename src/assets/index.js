bc7dd377735d177c5665c025467335d6932edf50
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2022-05-19 00:33:12 +0900

    Update src/assets/index.js
    Co-authored-by: Jiyeon Baek <58380158+100Gyeon@users.noreply.github.com>

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,7 @@ export { default as icHand } from './icons/ic_hand.svg';
 export { default as icTravel } from './icons/ic_travel.svg';
 export { default as icWish } from './icons/ic_wish.svg';
 export { default as icSearch } from './icons/ic_search.svg';
-export { default as icMassage } from './icons/ic_massage.svg';
+export { default as icMessage } from './icons/ic_message.svg';
 export { default as icProfile } from './icons/ic_profile.svg';
 export { default as imgChuncheon } from './images/img_chuncheon.svg';
 export { default as imgSokcho } from './images/img_sokcho.svg';
