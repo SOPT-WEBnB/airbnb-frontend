31703911dc7f7406904624fc8b8028c7738eb7ec
100Gyeon <bjy9966@naver.com>
2022-06-01 01:37:43 +0900

    chore: proxy 부분 삭제, baseURL 변경

--- src/libs/api.js
+++ src/libs/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const client = axios.create({
-  baseURL: '',
+  baseURL: 'http://13.209.22.56:3000/',
   headers: {
     'Content-Type': 'application/json',
   },
