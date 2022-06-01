2d2e4a802659edfdfc3b30b53e6c4b04b53aee08
100Gyeon <bjy9966@naver.com>
2022-06-02 03:09:25 +0900

    refactor: category 값 활용해서 active 여부 판단하고 스타일링

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,63 +1,40 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { IcWish, IcTravel, IcSearch, IcMessage, IcProfile } from 'assets';
-import { useNavigate } from 'react-router-dom';
-import React from 'react';
 
-function NavBar({ isActive }) {
+function NavBar({ category }) {
   const navigate = useNavigate();
 
   return (
-    <StyledFooter>
-      <StyledNavToggle>
+    <StyledNavBar>
+      <StyledNavButton>
         <IcSearch />
         <span>둘러보기</span>
-      </StyledNavToggle>
-
-      {isActive ? (
-        <>
-          <StyledNavToggle
-            onClick={() => {
-              navigate(`/wishlist`);
-            }}>
-            <IcWish />
-            <span>위시리스트</span>
-          </StyledNavToggle>
-          <StyledNavActive>
-            <IcTravel />
-            <span>여행</span>
-          </StyledNavActive>
-        </>
-      ) : (
-        <>
-          <StyledNavActive>
-            <IcWish />
-            <span>위시리스트</span>
-          </StyledNavActive>
-          <StyledNavToggle
-            onClick={() => {
-              navigate(`/`);
-            }}>
-            <IcTravel />
-            <span>여행</span>
-          </StyledNavToggle>
-        </>
-      )}
-
-      <StyledNavToggle>
+      </StyledNavButton>
+      <StyledNavButton onClick={() => navigate(`/wishlist`)} isActive={category === 'wishlist'}>
+        <IcWish />
+        <span>위시리스트</span>
+      </StyledNavButton>
+      <StyledNavButton onClick={() => navigate(`/`)} isActive={category === 'home'}>
+        <IcTravel />
+        <span>여행</span>
+      </StyledNavButton>
+      <StyledNavButton>
         <IcMessage />
         <span>메세지</span>
-      </StyledNavToggle>
-      <StyledNavToggle>
+      </StyledNavButton>
+      <StyledNavButton>
         <IcProfile />
         <span>프로필</span>
-      </StyledNavToggle>
-    </StyledFooter>
+      </StyledNavButton>
+    </StyledNavBar>
   );
 }
 
-export default React.memo(NavBar);
+export default NavBar;
 
-const StyledFooter = styled.footer`
+const StyledNavBar = styled.footer`
   position: sticky;
   bottom: 0;
   display: flex;
@@ -65,49 +42,37 @@ const StyledFooter = styled.footer`
   padding: 0.9rem 2.15rem 1rem 2.15rem;
   align-items: center;
   justify-content: space-between;
-
   height: 5.8rem;
-
   background: ${(props) => props.theme.colors.airWhite};
   border-top: 0.1rem solid ${(props) => props.theme.colors.airGray1};
 `;
 
-const StyledNavToggle = styled.div`
-  width: 4.4rem;
-  height: 3.9rem;
-
+const StyledNavButton = styled.button`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  width: 4.4rem;
+  font-size: 0.9rem;
   gap: 0.4rem;
-  flex-grow: 0;
-  cursor: pointer;
+  background: transparent;
 
-  height: 3.9rem;
-  color: ${(props) => props.theme.colors.airGray2};
-  img {
-    width: 2.4rem;
-    height: 2.4rem;
+  span {
+    color: ${(props) => (props.isActive ? props.theme.colors.airBlack : props.theme.colors.airGray2)};
+    font-weight: 600;
+  }
+
+  svg > path {
+    fill: ${(props) => props.isActive && props.theme.colors.airPink};
   }
 
   &:hover {
     span {
       color: ${(props) => props.theme.colors.airBlack};
-      font-weight: bold;
+      font-weight: 600;
     }
     svg > path {
       fill: ${(props) => props.theme.colors.airPink};
     }
-  }
-`;
-
-const StyledNavActive = styled(StyledNavToggle)`
-  span {
-    color: ${(props) => props.theme.colors.airBlack};
-    font-weight: bold;
-  }
-  svg > path {
-    fill: ${(props) => props.theme.colors.airPink};
   }
 `;
